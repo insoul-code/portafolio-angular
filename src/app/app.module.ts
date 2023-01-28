0a7c34e5d4c5297c499a7c15b8e0988c625a045f
Santiago Restrepo <santiago.restrepo@ceiba.com.co>
2023-01-28 15:03:30 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { DetailProjectComponent } from './components/detail-project/detail-project.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    BannerComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    DetailProjectComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
